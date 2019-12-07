0191996955918531220051ff3fd4bb13b824656e
Kelvin Liang <kelvin9877@gmail.com>
2019-12-07 22:18:32 +0800

    add browser env to linter config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,5 @@
+/*eslint-env browser*/
+
 module.exports = {
   parserOptions: { ecmaVersion: 6 },
   extends: "airbnb-base",
@@ -5,5 +7,8 @@ module.exports = {
     "no-shadow": 0,
     "no-param-reassign": 0,
     "eol-last": 0
+  },
+  env: {
+    browser: true
   }
 };
